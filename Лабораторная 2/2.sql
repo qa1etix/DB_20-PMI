7cef01a2242a9c26c91fb26a607fa87f9b79b4fb
Дмитрий <xmasbass666@gmail.com>
2025-09-25 14:45:33 +0300

    Лабораторная работа 2

---
+++ Лабораторная 2/2.sql
@@ -0,0 +1,50 @@
+drop table if exists calls cascade;
+drop table if exists transactions cascade;
+drop table if exists accounts cascade;
+drop table if exists city cascade;
+drop table if exists users cascade;
+
+create table users
+(
+   id bigint generated always as identity primary key,
+   fio text not null,
+   inn text check (length(inn) = 10) not null unique,
+   adress text not null,
+   legal_entity_name text
+);
+
+create table accounts
+(
+   id bigint primary key,
+   balance numeric(15,2) default(0.00),
+   debtor_status boolean default(false),
+   foreign key (id) references users(id) on delete cascade
+);
+
+create table transactions
+(
+   id bigint generated always as identity primary key,
+   account_id bigint not null,
+   amount numeric(15,2) not null,
+   transaction_type text check (transaction_type in ('income', 'expense')),
+   transaction_date timestamp default(current_timestamp),
+   foreign key (account_id) references accounts(id) on delete cascade
+);
+
+create table city
+(
+   city_id bigint generated always as identity primary key,
+   n_cost numeric(10,2) not null,
+   d_cost numeric(10,2) not null
+);
+
+create table calls
+(
+   call_id bigint generated always as identity primary key,
+   user_id bigint not null,
+   city_id bigint not null,
+   call_date timestamp default(current_timestamp),
+   duration interval not null,
+   foreign key (city_id) references city(city_id) on delete cascade,
+   foreign key (user_id) references users(id) on delete cascade
+);
